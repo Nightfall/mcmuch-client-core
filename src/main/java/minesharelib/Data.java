e90a80b9ad85b3009c628a8cee6b866f650869db
Striking <Strikingwolf2012@gmail.com>
2015-08-30 19:52:14 -0500

    Add getFrom and do some user stuff, next is putting values

--- src/main/java/minesharelib/Data.java
+++ src/main/java/minesharelib/Data.java
@@ -1,14 +1,24 @@
 package minesharelib;
 
 import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ContainerNode;
+import org.codehaus.jackson.node.ObjectNode;
 import org.codehaus.jackson.type.TypeReference;
 
 import java.io.IOException;
 import java.util.List;
 
 public abstract class Data {
+	private static <T, J extends ContainerNode> T get(String type, TypeReference<T> typeRef, Class<J> nodeType) throws IOException {
+		return API.mapper.readValue(API.INSTANCE.requester.get("/" + type, nodeType).asText(), typeRef);
+	}
+
 	protected static <T extends Data> List<T> getWith(String type, Class<T> clazz) throws IOException {
-		return API.mapper.readValue(API.INSTANCE.requester.get("/" + type, ArrayNode.class).asText(), new TypeReference<List<T>>() {});
+		return get(type, new TypeReference<List<T>>() {}, ArrayNode.class);
+	}
+
+	protected static <T extends Data> T getFrom(String type, Class<T> clazz) throws IOException {
+		return get(type, new TypeReference<T>() {}, ObjectNode.class);
 	}
 
 	public String toJson() throws IOException {
--- src/main/java/minesharelib/User.java
+++ src/main/java/minesharelib/User.java
@@ -1,9 +1,28 @@
 package minesharelib;
 
+import java.io.IOException;
+import java.util.List;
+
 public class User extends Data {
 	public final int id;
+	public final String hash;
+	public final String salt;
 
-	public User(int id) {
+	public static List<User> all() throws IOException {
+		return Data.getWith("users", User.class);
+	}
+
+	public static User fromId(int id) throws IOException {
+		return Data.getFrom("users/" + Integer.toString(id), User.class);
+	}
+
+	protected static User fromJson(String json) throws IOException {
+		return API.mapper.readValue(json, User.class);
+	}
+
+	public User(int id, String hash, String salt) {
 		this.id = id;
+		this.hash = hash;
+		this.salt = salt;
 	}
 }
