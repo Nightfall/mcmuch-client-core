672711be7db60f29e7d860afeba735f7ce361cd8
Striking <Strikingwolf2012@gmail.com>
2015-08-31 15:34:19 -0500

    Put

--- src/main/java/minesharelib/API.java
+++ src/main/java/minesharelib/API.java
@@ -1,12 +1,9 @@
 package minesharelib;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class API {
-	public static API INSTANCE = new API("http://mineshare.com");
-	public static final HttpClient client = HttpClients.createDefault();
+	public static final API INSTANCE = new API("http://mineshare.com");
 	public static final ObjectMapper mapper = new ObjectMapper();
 
 
--- src/main/java/minesharelib/Requester.java
+++ src/main/java/minesharelib/Requester.java
@@ -1,10 +1,16 @@
 package minesharelib;
 
 import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 
 class Requester {
+	private static final HttpClient client = HttpClients.createDefault();
 	private final HttpHost host;
 	public final String base;
 
@@ -13,7 +19,9 @@ class Requester {
 		this.base = host.getSchemeName() + host.getHostName() + "/api/v1";
 	}
 
-	public void put(String request, String json) throws IOException {
-		// TODO;
+	public HttpResponse put(String request, String json) throws IOException {
+		HttpPut put = new HttpPut(request);
+		put.setEntity(new StringEntity(json));
+		return client.execute(host, put);
 	}
 }
